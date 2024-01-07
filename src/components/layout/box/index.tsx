7f74845420e6f81075b97fbfe3b4adbe2966a3d7
IIIXwon <IIIXwon@gmail.com>
2024-01-07 21:28:57 +0900

    edit eslint rules

--- src/components/layout/box/index.tsx
+++ src/components/layout/box/index.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import {styled} from 'styled-components'
 import type {Responsive} from 'types/styles'
 import {toPropValue, Color, Space} from 'utils/style'
 
