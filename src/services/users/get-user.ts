333bbef2517add94fdd4836c89513351625c0fb7
IIIXwon <IIIXwon@gmail.com>
2024-01-07 18:35:56 +0900

    signin.ts, get-user.ts

---
+++ src/services/users/get-user.ts
@@ -0,0 +1,24 @@
+import {ApiContext, User} from 'types'
+import {fetcher} from '@/utils'
+
+export type GetUserParams = {
+    id: number
+}
+
+const getUser = async (
+    context: ApiContext,
+    {id} : GetUserParams
+): Promise<User> => {
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+        {
+            method: `GET`,
+            headers: {
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+            }
+        }
+    )
+}
+
+export default getUser
