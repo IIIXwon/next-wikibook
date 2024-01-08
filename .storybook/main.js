11cf7cc344b9aaa6de1818bb77e197f6212c3984
IIIXwon <IIIXwon@gmail.com>
2024-01-08 11:15:25 +0900

    storybook migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,12 +27,15 @@ module.exports = {
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-postcss',
   ],
+
   staticDir: ['public'],
+
   babel: async options => ({
     ...options,
     plugins: [
@@ -41,6 +44,7 @@ module.exports = {
       '@babel/plugin-transform-private-property-in-object'
     ],
   }),
+
   webpackFinal: async (config) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
@@ -48,5 +52,14 @@ module.exports = {
       })
     ];
     return config
+  },
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 }
